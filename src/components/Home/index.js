a7eb116636d5c18d835bbfe619246efe3eec6f32
Muhann <malhumai@purdue.edu>
2018-04-27 01:28:59 -0400

    home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,11 +4,15 @@ import { compose } from 'recompose';
 import withAuthorization from '../../withAuthorization';
 import { firebase } from '../../firebase';
 import './index.css'
-import { RaisedButton, Avatar, Paper, FlatButton, List, MenuItem, TextField,Divider } from 'material-ui';
+import { RaisedButton, Avatar, Paper, FlatButton, MenuItem, TextField,Divider, List, ListItem, Subheader  } from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import man from '../../images/man (1).png';
 import { auth, fs } from '../../firebase';
+import { transparent } from 'material-ui/styles/colors';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import { white } from 'material-ui/styles/colors';
+import { fullWhite } from 'material-ui/styles/colors';
 
 const muiTheme = getMuiTheme({
     "palette": {
@@ -30,7 +34,10 @@ const muiTheme = getMuiTheme({
           "selectedTextColor": "rgba(0, 0, 0, 0.26)",
           "hoverColor": "#616161",
           "rightIconDesktopFill": "rgba(0, 0, 0, 0.26)"
-      }
+      },
+  "listitem":{
+    "secondaryTextColor": "#ff5722",
+  }
   });
 
   const styles = {
@@ -62,7 +69,20 @@ const muiTheme = getMuiTheme({
     smallOne: {
       height: '5%',
       textAlign: 'center',
-    }
+    },
+
+    filler: {
+      height: '60%',
+      width: 800,
+      position: "fixed",
+      top: "50%",
+      left: "60%",
+      transform: "translate(-50%, -50%)",
+      textAlign: "center",
+
+      //display: 'in-line block',
+     overflow: "hidden",
+    },
   }
 class HomePage extends Component {
   constructor(props) {
@@ -72,6 +92,8 @@ class HomePage extends Component {
       users: null,
       displayName: '',
       interest: [],
+      avatarURL: '',
+      posts: [], 
     };
   }
 
@@ -93,12 +115,59 @@ class HomePage extends Component {
 
   }
 
+  makepost(tag, user, title, disc){
+
+   
+
+    var temp = 
+        <div>
+         <ListItem
+    
+    leftAvatar = {<RaisedButton label={tag} disabled={true} disabledBackgroundColor="#ffdc52" disabledLabelColor="#424242" />}
+    rightAvatar = {<RaisedButton label={user} disabled={true} disabledBackgroundColor="#ffdc52" disabledLabelColor="#424242" />}
+              
+        primaryText={
+                <p>{title}</p>
+    
+              }
+    
+              secondaryText={
+                
+      <p><span style={{color: white}}>
+                  {disc}
+             </span> </p>
+              }
+              secondaryTextLines={2}
+            />
+            <Divider inset={true} />;
+    </div>;
+
+  const arr = this.state.post.concat(temp);
+
+  this.setState({
+    post: arr
+  });
+    
+  }
+
   render() {
 
       const { users} = this.state;
+      let {avatarURL} = this.state;
+      let avURL = null;
+      if(avatarURL){
+        avURL = (<img className='resize' src={this.state.avatarURL} />)
+      }else{
+        fs.getUser(this.props.authUser.uid).then(doc => {
+          this.setState({
+            avatarURL: doc.data().photoURL
+          })
+        })
+        avURL = (<img className='resize' src = {this.state.avatarURL} />)
+      }
         console.log(this.state.interest)
       const interestList = this.state.interest.map((inter)=>{
-          return <RaisedButton label={inter} disabled={true} fullWidth={true} disabledBackgroundColor={"#ffdc52"} disabledLabelColor={"#424242"}/>
+          return <RaisedButton label={inter} disabled={true} fullWidth={true} disabledBackgroundColor={"#ffdc52"} disabledLabelColor={"#424242"} label={inter} labelColor={"#424242"}/>
       })
       console.log(interestList)
     return (
@@ -109,7 +178,7 @@ class HomePage extends Component {
               <Paper style={styles.paper}>
                   <Paper style={styles.insidepaper} zDepth = {0}>
                     <br/>
-                    <img className='resize' src={man}/>
+                    {avURL}
                   </Paper>
                     <br/>
                    <Divider inset={true} style={styles.divider}/>
@@ -126,8 +195,19 @@ class HomePage extends Component {
                     </div>
                 </Paper>
             </div>
-            <div className='column'>
-                <h1>Chicken cker</h1>
+            <div>
+               <Paper style={styles.filler}>
+               
+             <h1>Feed</h1>
+            
+               <div className="scroll">
+               <List>
+              
+              </List>
+
+                 </div>
+
+               </Paper>
             </div>
         </div>
       </div>
